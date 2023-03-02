e86fe03af8648aaf783ed5e92b884968970c60f6
Riley <riley.freier@gmail.com>
2023-03-02 14:34:47 -0700

    little block man can now move

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// Your code here  
+
+//move left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+        
+  if ( left > 0 ) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+//move right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if ( left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+//event listeners
+document.addEventListener('keydown', function(e) {
+  if ( e.which === 37 ) {
+    moveDodgerLeft();
+  }
+  if ( e.which === 39 ) {
+    moveDodgerRight();
+  }
+});
